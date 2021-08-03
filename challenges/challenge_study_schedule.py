229064364d3b4755054782c112fefc27dc0cbfe5
bruno-alves7 <bruno.spawnhell@gmail.com>
2021-08-02 23:05:37 -0300

    Optimization item 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,13 @@
 def study_schedule(permanence_period, target_time):
     """verifica a quant. que um valor aparece em uma lista de tuplas"""
 
+    if not isinstance(target_time, int):
+        return None
     count = 0
     for element in permanence_period:
         if (
             not isinstance(element[0], int)
             or not isinstance(element[1], int)
-            or not isinstance(target_time, int)
         ):
             return None
         if element[0] <= target_time <= element[1]:
