5c26148818bb055181643546dc960adf688506ed
bruno-alves7 <bruno.spawnhell@gmail.com>
2021-08-03 23:35:03 -0300

    item 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,42 @@
+def quickSort(myList, start, end):
+    if start < end:
+        pivot = partition(myList, start, end)
+        quickSort(myList, start, pivot - 1)
+        quickSort(myList, pivot + 1, end)
+    return myList
+
+
+def partition(myList, start, end):
+    pivot = myList[start]
+    left = start + 1
+    right = end
+    done = False
+    while not done:
+        while left <= right and myList[left] <= pivot:
+            left = left + 1
+        while myList[right] >= pivot and right >= left:
+            right = right - 1
+        if right < left:
+            done = True
+        else:
+            temp = myList[left]
+            myList[left] = myList[right]
+            myList[right] = temp
+    temp = myList[start]
+    myList[start] = myList[right]
+    myList[right] = temp
+    return right
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    """ verifica se duas palavras são anagramas """
+    if first_string == "" or second_string == "":
+        return False
+    if len(first_string) != len(second_string):
+        return False
+
+    return quickSort(
+        list(first_string), 0, len(first_string) - 1
+    ) == quickSort(list(second_string), 0, len(second_string) - 1)
+
+# https://codezup.com/quick-sort-implementation-example-in-python/
